10e019bb63bac61300228af1ba6394264cbb9c81
tooPieElf <ndimnezechukwudi@gmail.com>
2021-08-20 14:22:38 +0100

    add Api response;

---
+++ src/main/java/com/digicore/digicore/apiResponse/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.digicore.digicore.apiResponse;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ApiResponse {
+    int responseCode;
+    Boolean successResponse;
+    String message;
+    Object account;
+}
